eceead85f33ab681fa179ca18f90c3ec641fc4ef
mjboothaus <mjboothaus@gmail.com>
2017-09-25 15:51:46 -0700

    Initial commit of code for Week 3 MapReduce (MR) miniproj

---
+++ word_count_mr.py
@@ -0,0 +1,29 @@
+
+from mrjob.job import MRJob
+import re
+
+WORD_RE = re.compile(r"\w+")
+
+
+class MRWordCount(MRJob):
+    def mapper(self, _, line):
+        for word in WORD_RE.findall(line):
+            yield (word.lower(), 1)
+
+    def reducer(self, word, counts):
+        yield (word, sum(counts))
+
+
+# TODO: Adjust following class to return top 100 pairs
+
+class MRTop100(MRJob):
+    def mapper(self, _, line):
+        for word in WORD_RE.findall(line):
+            yield (word.lower(), 1)
+
+    def reducer(self, word, counts):
+        yield (word, sum(counts))
+
+
+if __name__ == '__main__':
+    MRWordCount.run()
