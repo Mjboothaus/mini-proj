ddf0863d813e9ce3776d85b28e0e36a0a9606d13
mjboothaus <mjboothaus@gmail.com>
2017-09-25 20:34:00 -0700

    Attempt to code up first question

--- word_count_mr.py
+++ word_count_mr.py
@@ -1,6 +1,8 @@
-
 from mrjob.job import MRJob
 import re
+#from mrjob.compat import get_jobconf_value
+from heapq import heappush, heappop
+
 
 WORD_RE = re.compile(r"\w+")
 
@@ -14,15 +16,31 @@ class MRWordCount(MRJob):
         yield (word, sum(counts))
 
 
-# TODO: Adjust following class to return top 100 pairs
+# Second job takes a collection of pairs (word, count) and filter for only the highest N e.g. 100
+
+class MRTopN(MRJob):
+
+    def __init__(self):
+        #num = get_jobconf_value("my.job.settings.num")
+        self.num = 100
+        self.h = []
+        return self
+
+    def mapper(self, key, value):
+        heappush(self.h, (value, key))
+        return self
+
+    def reducer(self):
+        i = 0
+        while i < self.num:
+            yield heappop(self.h)
 
 class MRTop100(MRJob):
-    def mapper(self, _, line):
-        for word in WORD_RE.findall(line):
-            yield (word.lower(), 1)
 
-    def reducer(self, word, counts):
-        yield (word, sum(counts))
+    # Combine the word count and top N classes
+
+    def steps(self):
+        return MRWordCount.steps() + MRTopN.steps()
 
 
 if __name__ == '__main__':
